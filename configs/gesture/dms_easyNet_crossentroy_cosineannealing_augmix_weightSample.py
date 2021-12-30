da1ce4acd8319a1c18d5528e49e73c75ddaaccb5
姜涛 <jiangtao2@streamax.com>
2021-12-30 09:10:49 +0800

    添加weight sample配置文件

---
+++ configs/gesture/dms_easyNet_crossentroy_cosineannealing_augmix_weightSample.py
@@ -0,0 +1,37 @@
+# 可复现性
+reproductive = True
+# 数据集路径
+imgDirTrain = '/home/jiangtao/dataset/train/Gesture_static' 
+imgTxtTrain = 'train.txt'
+imgDirValid = '/home/jiangtao/dataset/train/Gesture_static'
+imgTxtValid = 'val.txt'
+
+# 训练用到的数字
+batchSize = 512 
+workers = 32 
+imgSize = 128 
+numEpoches = 200
+validInterval = 2 
+savingEpoches = 10 
+
+# 数据增强
+augmix_alpha = 1.0
+weightSample = True
+
+# 模型
+channels = 1
+modelName = 'easyNet'
+pretrained = True
+numClasses = 3
+
+# 损失
+lossName = 'crossEntroy'
+
+# 优化器参数设定
+momentum = 0.9
+weightdecay = 0.0005
+scheduler = 'cosineAnnealing'
+
+# model和log路径
+log_path = None
+saved_path = None
